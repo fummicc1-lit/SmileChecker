780211b730409a950b46a47dfa0c4ea902a18a7c
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-14 22:12:36 +0900

    Initial Commit

---
+++ SmileBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmileBook
+//
+//  Created by Fumiya Tanaka on 2022/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
