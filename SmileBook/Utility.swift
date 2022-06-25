2079bf2458348f471c44ea120effa0a2044bb75c
fummicc1 <fumiya.tennis1@gmail.com>
2022-06-25 12:03:47 +0900

    カメラバージョン

--- SmileBook/Utility.swift
+++ SmileBook/Utility.swift
@@ -3,38 +3,27 @@ import UIKit
 import CoreML
 import Vision
 
-extension Person {
-    static func create(name: String) -> Person {
-        Person(name: name)
-    }
-}
-
-func checkHasSmile(
-    imageName: String,
-    completion: @escaping (Bool) -> Void
+func checkSmile(
+    image: CGImage,
+    completion: @escaping (Float) -> Void
 ) {
-    let positiveThreshold: VNConfidence = 0.85
-    let negativeThreshold: VNConfidence = 0.05
-    let image = UIImage(named: imageName)!
     let classifier = try! SmileChecker(configuration: .init())
     let model = try! VNCoreMLModel(for: classifier.model)
     let request = VNCoreMLRequest(model: model) { request, error in
         if let results = request.results as? [VNClassificationObservation] {
-            var nonSmile: VNClassificationObservation?
             var smile: VNClassificationObservation?
             for result in results {
                 if result.identifier == "smile" {
                     smile = result
-                } else {
-                    nonSmile = result
                 }
             }
-            let hasSmile = smile!.confidence > positiveThreshold && nonSmile!.confidence < negativeThreshold
-            completion(hasSmile)
+            if let smile = smile {
+                completion(smile.confidence)
+            }
         }
     }
 
-    let handler = VNImageRequestHandler(cgImage: image.cgImage!)
+    let handler = VNImageRequestHandler(cgImage: image)
 
     try! handler.perform([request])
 }
--- SmileBook/ViewController.swift
+++ SmileBook/ViewController.swift
@@ -1,67 +1,133 @@
 import UIKit
+import AVFoundation
+import AVKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var smileLabel: UILabel!
-
-    var index: Int = 0
-
-    var people: [Person] = []
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // 全部で8つの要素（人の名前は画像名に一致しているよ）
-        people = [
-            Person.create(name: "Allisa"),
-            Person.create(name: "Ann"),
-            Person.create(name: "Ben"),
-            Person.create(name: "Cassie"),
-            Person.create(name: "Jack"),
-            Person.create(name: "Julia"),
-            Person.create(name: "Mike"),
-            Person.create(name: "Rola")
-        ]
-        // MARK: UIを変更する
-        if people[index].hasSmile == true {
-            smileLabel.text = "\(people[index].name) smiles."
-        } else {
-            smileLabel.text = "\(people[index].name) does not smile."
-        }
-        imageView.image = UIImage(named: people[index].name)
-    }
-
-    @IBAction func next() {
-        // 一番最後まで来たかチェック
-        if index == 7 {
-            // indexを先頭に戻す
-            index = 0
-        } else {
-            // indexを+1する
-            index += 1
-        }
-        // MARK: UIを変更する
-        if people[index].hasSmile == true {
-            smileLabel.text = "\(people[index].name) smiles."
-        } else {
-            smileLabel.text = "\(people[index].name) does not smile"
-        }
-        imageView.image = UIImage(named: people[index].name)
-    }
-
-    @IBAction func back() {
-        if index == 0 {
-            index = 7
-        } else {
-            index -= 1
-        }
-        // MARK: UIを変更する
-        if people[index].hasSmile == true {
-            smileLabel.text = "\(people[index].name) smiles."
-        } else {
-            smileLabel.text = "\(people[index].name) does not smile."
-        }
-        imageView.image = UIImage(named: people[index].name)
+class PlayerView: UIView {
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
     }
 }
 
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    @IBOutlet var smileLabel: UILabel!
+    @IBOutlet var previewView: PlayerView!
+
+    var session: AVCaptureSession!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    var term: Int = 0
+    let interval: Int = 100
+    var bestValue: Float = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initCamera()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+
+        self.session.stopRunning()
+
+        for output in self.session.outputs {
+            self.session.removeOutput(output as AVCaptureOutput)
+        }
+        for input in self.session.inputs {
+            self.session.removeInput(input as AVCaptureInput)
+        }
+        self.session = nil
+    }
+
+    private func initCamera() {
+
+        guard let caputureDevice = AVCaptureDevice.default(
+            .builtInWideAngleCamera,
+            for: AVMediaType.video,
+            position: .front
+        ) else {
+            print("カメラにアクセスできません")
+            return
+        }
+
+        caputureDevice.activeVideoMinFrameDuration = CMTimeMake(value: 1, timescale: 30)
+
+        do {
+            let input = try AVCaptureDeviceInput(device: caputureDevice)
+            let output: AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
+            output.videoSettings = [
+                kCVPixelBufferPixelFormatTypeKey : kCVPixelFormatType_32BGRA
+            ] as [String : Any]
+            output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+            output.alwaysDiscardsLateVideoFrames = true
+
+            self.session = AVCaptureSession()
+            self.session.sessionPreset = .medium
+
+            if self.session.canAddInput(input) && self.session.canAddOutput(output) {
+                self.session.addInput(input)
+                self.session.addOutput(output)
+
+                self.startPreview()
+            }
+        }
+        catch _ {
+            print("error occurd")
+        }
+    }
+
+    private func startPreview() {
+
+        self.videoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+
+        self.videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+
+        self.videoPreviewLayer.connection?.videoOrientation = .portrait
+
+
+        self.view.layer.addSublayer(videoPreviewLayer)
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.session.startRunning()
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewView.bounds
+            }
+        }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("1フレームごとの処理をここに書く")
+        guard let buffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        let image = CIImage(cvImageBuffer: buffer)
+        let imageRect = CGRect(
+            origin: .zero,
+            size: CGSize(
+                width: CVPixelBufferGetWidth(buffer),
+                height: CVPixelBufferGetHeight(buffer)
+            )
+        )
+        let context = CIContext()
+        guard let image = context.createCGImage(image, from: imageRect) else {
+            return
+        }
+        term += 1
+        if term % interval == 0 {
+            bestValue = 0
+            term = 0
+        }
+        checkSmile(image: image) { smileConfidence in
+            self.bestValue = max(smileConfidence, self.bestValue)
+            let smile = String(format: "%.2f", self.bestValue)
+            DispatchQueue.main.async {
+                self.smileLabel.text = smile
+            }
+        }
+    }
+
+}
